28968744a59b8a33950a00cb4b2596f7455675c0
OptoMedEd-Git <161382971+OptoMedEd-Git@users.noreply.github.com>
2024-08-08 18:31:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ sanity/schemas/modules/custom-html.ts
@@ -0,0 +1,38 @@
+import { defineField, defineType } from 'sanity'
+import { VscCode } from 'react-icons/vsc'
+
+export default defineType({
+	name: 'custom-html',
+	title: 'Custom HTML',
+	icon: VscCode,
+	type: 'object',
+	fields: [
+		defineField({
+			name: 'uid',
+			title: 'Unique Identifier',
+			type: 'uid',
+		}),
+		defineField({
+			name: 'className',
+			type: 'string',
+		}),
+		defineField({
+			name: 'html',
+			title: 'HTML',
+			type: 'code',
+			options: {
+				language: 'html',
+				languageAlternatives: [{ title: 'HTML', value: 'html' }],
+			},
+		}),
+	],
+	preview: {
+		select: {
+			code: 'html.code',
+		},
+		prepare: ({ code }) => ({
+			title: code,
+			subtitle: 'Custom HTML',
+		}),
+	},
+})
